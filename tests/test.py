6fe554c5f100e9e38ff33dc5194caa43c1a749d0
conor <conor.waldron@gmail.com>
2023-06-05 14:07:04 +0100

    team tied

--- tests/test.py
+++ tests/test.py
@@ -94,38 +94,21 @@ def team_2_subs_and_lower_class_df():
 
 
 @pytest.fixture(scope='session')
-def prev_week1_team_4():
+def prev_week1():
     prev_week1 = pd.read_csv('after_week1.csv')
-    prev_week1_team_4 = prev_week1[prev_week1['Team'] == 4]
-    return prev_week1_team_4
+    return prev_week1
 
 
 @pytest.fixture(scope='session')
-def prev_week2_team_4():
+def prev_week2():
     prev_week2 = pd.read_csv('after_week2.csv')
-    prev_week2_team_4 = prev_week2[prev_week2['Team'] == 4]
-    return prev_week2_team_4
+    return prev_week2
 
 
 @pytest.fixture(scope='session')
-def prev_week2_team_3():
-    prev_week2 = pd.read_csv('after_week2.csv')
-    prev_week2_team_3 = prev_week2[prev_week2['Team'] == 3]
-    return prev_week2_team_3
-
-
-@pytest.fixture(scope='session')
-def prev_week2_team_2():
-    prev_week2 = pd.read_csv('after_week2.csv')
-    prev_week2_team_2 = prev_week2[prev_week2['Team'] == 2]
-    return prev_week2_team_2
-
-
-@pytest.fixture(scope='session')
-def prev_week3_team_7():
+def prev_week3():
     prev_week3 = pd.read_csv('after_week3.csv')
-    prev_week3_team_7 = prev_week3[prev_week3['Team'] == 7]
-    return prev_week3_team_7
+    return prev_week3
 
 
 @pytest.fixture(scope='session')
@@ -365,28 +348,28 @@ def test_has_7_unique_reg_players(proposed_team_6_players, proposed_team_same_pl
 
 #@pytest.mark.skip
 def test_team_played_before(valid_team_4, reg_team_4_wrong_order, team_4_same_as_week2_wrong_order,
-                            team_4_df, team_4_subs_and_lower_class_df, prev_week1_team_4, prev_week2_team_4):
+                            team_4_df, team_4_subs_and_lower_class_df, prev_week1, prev_week2):
     """
     Checks that
     if your 7 players are your registered 7 that they play in those positions
     if your 7 players have all played on same day as before that they play in the same positions
     """
     expected_value = True, None
-    actual_value = team_played_before(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = team_played_before(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, "you are using the same 7 players as your registered team but Andrew Synnott is not playing in the registered position of D2"
-    actual_value = team_played_before(reg_team_4_wrong_order, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = team_played_before(reg_team_4_wrong_order, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, "you are using the same 7 players as week 2 team but James Fagan is not playing in the old position of S3"
-    actual_value = team_played_before(team_4_same_as_week2_wrong_order, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = team_played_before(team_4_same_as_week2_wrong_order, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
 
 # @pytest.mark.skip
 def test_singles_right_order(valid_team_4, team_4_singles_out_order, team_4_singles_out_order_class, team_4_singles_out_order_team,
-                             team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4):
+                             team_4_df, team_4_subs_and_lower_class_df, prev_week2):
     """
     Checks that
     you never play a singles player above someone who has played before them before
@@ -394,80 +377,79 @@ def test_singles_right_order(valid_team_4, team_4_singles_out_order, team_4_sing
     you never play a singles player above someone from a higher team
     """
     expected_value = True, None
-    actual_value = singles_right_order(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = singles_right_order(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, 'You are playing Shane Bergin ahead of Conor Waldron but that violates a previous weeks order'
-    actual_value = singles_right_order(team_4_singles_out_order, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = singles_right_order(team_4_singles_out_order, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, 'You are playing Brian Masterson (class 5) ahead of Conor Waldron (class 4)'
-    actual_value = singles_right_order(team_4_singles_out_order_class, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = singles_right_order(team_4_singles_out_order_class, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, 'You are playing James Fagan (team 5) ahead of Conor Waldron (team 4)'
-    actual_value = singles_right_order(team_4_singles_out_order_team, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = singles_right_order(team_4_singles_out_order_team, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
 
 # @pytest.mark.skip
 def test_doubles_team_and_class_checker(valid_team_4, team_4_invalid_doubles_class, team_4_invalid_doubles_team,
-                             team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4):
+                             team_4_df, team_4_subs_and_lower_class_df, prev_week2):
     """
     Checks that
     you never play a doubles player above someone of a better class
     you never play a doubles player above someone from a higher team
     """
     expected_value = True, None
-    actual_value = doubles_team_and_class_checker(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = doubles_team_and_class_checker(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, 'Your 2nd doubles contains a class 4 player which is better than one of the players on your first doubles (class 5)'
-    actual_value = doubles_team_and_class_checker(team_4_invalid_doubles_class, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = doubles_team_and_class_checker(team_4_invalid_doubles_class, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, 'Your 2nd doubles contains a player registered to team 4 which is better than one of the players on your first doubles (registered to team 5)'
-    actual_value = doubles_team_and_class_checker(team_4_invalid_doubles_team, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = doubles_team_and_class_checker(team_4_invalid_doubles_team, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
 
 # @pytest.mark.skip
 def test_doubles_right_order(valid_team_4, team_3_invalid_doubles_order, team_2_invalid_doubles_order,
-                             team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4,
-                             team_3_df, team_3_subs_and_lower_class_df, prev_week2_team_3,
-                             team_2_df, team_2_subs_and_lower_class_df, prev_week2_team_2):
+                             team_4_df, team_4_subs_and_lower_class_df, prev_week2,
+                             team_3_df, team_3_subs_and_lower_class_df,
+                             team_2_df, team_2_subs_and_lower_class_df):
     """
     Checks that
     you never play a doubles player above someone who has played before them before
     you never play a 2nd doubles reg team ahead of a 1st doubles reg team
     """
     expected_value = True, None
-    actual_value = doubles_right_order(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = doubles_right_order(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, 'Your 2nd doubles pairing have played above your first doubles pairing in a previous week'
-    actual_value = doubles_right_order(team_2_invalid_doubles_order, team_2_df, team_2_subs_and_lower_class_df, prev_week2_team_2)
+    actual_value = doubles_right_order(team_2_invalid_doubles_order, team_2_df, team_2_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
     expected_value = False, 'all 4 registered players are playing doubles, but not in the right order'
-    actual_value = doubles_right_order(team_3_invalid_doubles_order, team_3_df, team_3_subs_and_lower_class_df, prev_week2_team_3)
+    actual_value = doubles_right_order(team_3_invalid_doubles_order, team_3_df, team_3_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
 
 # @pytest.mark.skip
-def test_team_tied(valid_team_4, valid_team_7, invalid_team_7_team_tied,
-                   team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4,
-                   team_7_df, team_7_subs_and_lower_class_df, prev_week3_team_7):
+def test_team_tied(valid_team_4, valid_team_7, invalid_team_7_team_tied, prev_week2, prev_week3,
+                   team_4_df, team_4_subs_and_lower_class_df,
+                   team_7_df, team_7_subs_and_lower_class_df):
     expected_value = True, None
-    actual_value = team_tied(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2_team_4)
+    actual_value = team_tied(valid_team_4, team_4_df, team_4_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
-    expected_value = ?
-    actual_value = team_tied(valid_team_7, team_7_df, team_7_subs_and_lower_class_df, prev_week3_team_7)
+    expected_value = True, None
+    actual_value = team_tied(valid_team_7, team_7_df, team_7_subs_and_lower_class_df, prev_week2)
     assert expected_value == actual_value
 
-    # warning something strange, kabir is team tied after week 3 but rule is not trigger here!
+    expected_value = False, 'Kabir Kalia is inelgiible due to team tying, he has played for a higher team on 2 occasions'
+    actual_value = team_tied(valid_team_7, team_7_df, team_7_subs_and_lower_class_df, prev_week3)
+    assert expected_value == actual_value
 
-    #expected_value = False, '{player} is inelgiible due to team tying, he has played for a higher team on {len(higher_teams)} occasions'
-    #actual_value = team_tied(invalid_team_7_team_tied, team_7_df, team_7_subs_and_lower_class_df, prev_week3_team_7)
-    #assert expected_value == actual_value
